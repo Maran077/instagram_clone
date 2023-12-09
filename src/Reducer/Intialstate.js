c9a9896208d68b03a48b8c101734526fff280116
Maran077 <marvelmaran07@gmail.com>
2023-12-09 12:03:34 +0530

    second commit

--- src/Reducer/Intialstate.js
+++ src/Reducer/Intialstate.js
@@ -1,5 +1,5 @@
 import { db } from "../firebase";
-import { collection, getDocs, query, where } from "firebase/firestore";
+import { collection, getDocs, query, where } from "firebase/firestore"; 
 
 import Cookies from "js-cookie";
 
@@ -7,26 +7,22 @@ import Cookies from "js-cookie";
 export let initialstate
 
 let user = Cookies.get("user")
-
-try {
-  if (!user) return alert("please login first");
-  user = JSON.parse(user)
-  console.log(user.uid);
-  const q = query(collection(db, "users"), where("uid", "==", user.uid))
-  const snapshot = await getDocs(q)
-  snapshot.forEach((doc) => {
-    initialstate = doc.data()
-    console.log(doc.id, " => ", doc.data());
-  });
-
-} catch (error) {
-  alert(error.code || "error!")
-}
-
-
-
-
-
-
-
-
+async function getUserData() {
+  
+  try {
+    if (!user) return alert("please login first");
+      user = JSON.parse(user)
+      console.log(user.uid);
+      const q = query(collection(db, "users"), where("uid", "==", user.uid))
+      const snapshot = await getDocs(q)
+      snapshot.forEach((doc) => {
+        initialstate = doc.data()
+        console.log(doc.id, " => ", doc.data());
+      });
+    
+  } catch (error) {
+    alert(error.code||"error!")
+  }
+  
+}  
+getUserData
